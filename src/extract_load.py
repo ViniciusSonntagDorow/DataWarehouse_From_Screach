02e0c79d385aa8d870981bb87ba41d224b3da9de
Vinicius Sonntag Dorow <vinicius.s.dorow@gmail.com>
2024-07-26 07:49:12 -0300

    feature extract_load

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,10 +1,26 @@
+#Imports
 import yfinance as yf
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+#Env Variables
+
+load_dotenv()
+
 commodities = ['CL=F', 'GC=F', 'SI=F']
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_engine(DATABASE_URL)
 
 def get_commodities_data(symbol,period='5y',interval='1d'):
     ticker = yf.Ticker(symbol)
@@ -19,6 +35,9 @@ def get_all_commodities_data(commodities):
         all_data.append(data)
     return pd.concat(all_data)
 
+def to_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
 if __name__ == '__main__':
     concat_data = get_all_commodities_data(commodities=commodities)
-    concat_data
+    to_postgres(concat_data)
